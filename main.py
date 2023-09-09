4b4bb31b9b986c9411c6d573a97b287f02d55f42
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-09-09 18:02:53 +0000

    handled cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from datetime import datetime, timedelta
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app=FastAPI()
+
+origins = ["*"]
+app.add.middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)   
 
 
 @app.get("/")
