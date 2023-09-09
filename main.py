24d21e641b04c4091e8d3c961399f674a99ce86e
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-09-09 18:07:52 +0000

    Update main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
 
 origins = ["*"]
-app.add.middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
